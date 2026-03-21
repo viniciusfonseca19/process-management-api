614946da64d9b0f1399c713dc35a8188364c8c50
Vinícius F. <fonsecavinicius117@gmail.com>
2026-03-20 21:35:44 -0300

    Correção do response

--- src/main/java/com/process/api/application/dto/response/ProcessResponseDTO.java
+++ src/main/java/com/process/api/application/dto/response/ProcessResponseDTO.java
@@ -1,4 +1,21 @@
 package com.process.api.application.dto.response;
 
+import com.process.api.domain.enums.ProcessStatus;
+import com.process.api.domain.enums.ProcessType;
+import lombok.Builder;
+import lombok.Data;
+
+import java.time.LocalDateTime;
+
+@Data
+@Builder
 public class ProcessResponseDTO {
+
+    private Long id;
+    private ProcessType type;
+    private ProcessStatus status;
+    private String payload;
+    private String result;
+    private LocalDateTime createdAt;
+    private LocalDateTime updatedAt;
 }
