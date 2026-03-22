c3567e782c2b64ccb313f0667a879d6d017e9c6b
Vinícius F. <fonsecavinicius117@gmail.com>
2026-03-22 13:09:52 -0300

    AuthController base de interfaces

--- src/main/java/com/process/api/interfaces/controller/AuthController.java
+++ src/main/java/com/process/api/interfaces/controller/AuthController.java
@@ -1,4 +1,13 @@
 package com.process.api.interfaces.controller;
 
+import org.springframework.web.bind.annotation.*;
+
+@RestController
+@RequestMapping("/auth")
 public class AuthController {
+
+    @PostMapping("/login")
+    public String login() {
+        return "Login não implementado ainda";
+    }
 }
