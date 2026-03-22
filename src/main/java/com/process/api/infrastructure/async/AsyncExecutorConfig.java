135040d0bd2e3d9626e526d22fd53e45081f7b95
Vinícius F. <fonsecavinicius117@gmail.com>
2026-03-22 13:03:17 -0300

    finalização correta do Async

--- src/main/java/com/process/api/infrastructure/async/AsyncExecutorConfig.java
+++ src/main/java/com/process/api/infrastructure/async/AsyncExecutorConfig.java
@@ -1,4 +1,22 @@
 package com.process.api.infrastructure.async;
 
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.scheduling.concurrent.ThreadPoolTaskExecutor;
+
+import java.util.concurrent.Executor;
+
+@Configuration
 public class AsyncExecutorConfig {
+
+    @Bean(name = "taskExecutor")
+    public Executor taskExecutor() {
+        ThreadPoolTaskExecutor executor = new ThreadPoolTaskExecutor();
+        executor.setCorePoolSize(5);
+        executor.setMaxPoolSize(10);
+        executor.setQueueCapacity(50);
+        executor.setThreadNamePrefix("process-exec-");
+        executor.initialize();
+        return executor;
+    }
 }
