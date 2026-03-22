6e05ec7885bd2bb0bc95d247e492d85aec2ef9a5
Vinícius F. <fonsecavinicius117@gmail.com>
2026-03-22 13:07:44 -0300

    finalização base dos config

--- src/main/java/com/process/api/infrastructure/logging/LoggingConfig.java
+++ src/main/java/com/process/api/infrastructure/logging/LoggingConfig.java
@@ -1,4 +1,8 @@
 package com.process.api.infrastructure.logging;
 
+import org.springframework.context.annotation.Configuration;
+
+@Configuration
 public class LoggingConfig {
+    // Pode configurar logback futuramente
 }
