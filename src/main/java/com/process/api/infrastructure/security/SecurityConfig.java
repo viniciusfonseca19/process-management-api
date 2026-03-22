615cfa894ec869cd16c537f5ab71fec5fcf81720
Vinícius F. <fonsecavinicius117@gmail.com>
2026-03-22 13:09:03 -0300

    SecurityConfig libere API temporariamente

--- src/main/java/com/process/api/infrastructure/security/SecurityConfig.java
+++ src/main/java/com/process/api/infrastructure/security/SecurityConfig.java
@@ -2,23 +2,20 @@ package com.process.api.infrastructure.security;
 
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
-import org.springframework.security.config.Customizer;
-import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
-import org.springframework.security.web.SecurityFilterChain;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.web.SecurityFilterChain;
 
 @Configuration
-@EnableWebSecurity
 public class SecurityConfig {
 
     @Bean
     public SecurityFilterChain securityFilterChain(HttpSecurity http) throws Exception {
+
         http
                 .csrf(csrf -> csrf.disable())
                 .authorizeHttpRequests(auth -> auth
                         .anyRequest().permitAll()
-                )
-                .httpBasic(Customizer.withDefaults());
+                );
 
         return http.build();
     }
